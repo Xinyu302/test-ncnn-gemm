9b6afc273393710443ce570a4dc9f2d6dcfc8550
yxy <summerhh54@gmail.com>
2023-08-19 16:14:53 +0800

    modify transform for gemm

--- riscv/transform.py
+++ riscv/transform.py
@@ -1,7 +1,7 @@
 import re
 
 
-with open("test_pack_B_handle.cpp", "r") as f:
+with open("test_gemm_transB_packed_tile.cpp", "r") as f:
     content = f.read()
 
 def replace_val_pattern(text):
@@ -32,11 +32,17 @@ def replace_vld4q_f32_pattern(text):
 def replace_type_float32x4_t(text):
     return text.replace("float32x4_t", "vfloat32m1_t").replace("float32x4x4_t", "vfloat32m1x4_t")
 
+def replace_vfmaq_f32(text):
+    pattern = r'vfmaq_f32\(([^,]+), ([^,]+), ([^)]+)\);'
+    replacement = r'vfmadd_vv_f32m1(\2, \3, \1, VL);'
+    new_content = re.sub(pattern, replacement, text)
+    return new_content
+
 new_content = replace_val_pattern(content)
 new_content = replace_type_float32x4_t(new_content)
 new_content = replace_vld1q_pattern(new_content)
 new_content = replace_vst1q_pattern(new_content)
 new_content = replace_vld4q_f32_pattern(new_content)
 
-with open("test_pack_B_handle.cpp", "w") as f:
+with open("test_gemm_transB_packed_tile.cpp", "w") as f:
     f.write(new_content)
